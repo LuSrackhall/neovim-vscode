1479c6039d723e114a7cd7c15d899b88f3fac838
LuSrackhall <3647637206@qq.com>
2025-02-25 14:13:32 +0800

    feat(neovide): 本次配置使得在neovide中, 可以通过F11切换全屏模式。

--- init.lua
+++ init.lua
@@ -6,3 +6,10 @@ if vim.g.vscode then
 else
   -- ordinary Neovim
 end
+
+if vim.g.neovide then
+  vim.keymap.set("n", "<F11>", function()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+  end, { silent = true })
+end
+
